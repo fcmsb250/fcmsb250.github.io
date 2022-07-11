89420320e3d1d7e03de893855c410586940ea2f3
智障收容所(反防沉迷) <84607814+dsy4567@users.noreply.github.com>
2022-07-11 22:24:40 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,34 @@
+/*
+Copyright (C) 2022 dsy4567 <https://github.com/dsy4567 | dsy4567@outlook.com>
+此代码在 GPL-3.0 下获得许可
+*/
+window.onerror = async () => {
+    try {
+        加载界面.hide();
+    } catch (e) {
+        window.onerror = null;
+    }
+};
+window.onload = async () => {
+    导航栏.style.backgroundColor = "var(--theme-color1)";
+    导航栏.style.boxShadow = "none";
+    加载界面.style.animationName = "加载界面";
+    setTimeout(() => {
+        加载界面.remove();
+    }, 500);
+};
+document.body.onscroll = async () => {
+    if (document.documentElement.scrollTop == 0) {
+        导航栏.style.backgroundColor = "var(--theme-color1)";
+        导航栏.style.boxShadow = "none";
+    } else {
+        导航栏.style.backgroundColor = "var(--theme-color3)";
+        导航栏.style.boxShadow = "0 12px 16px 0 var(--theme-color3)";
+    }
+};
+
+async () => {
+    document.querySelectorAll("a.外链").forEach((e) => {
+        e.setAttribute("target", "_blank");
+    });
+};
---
+++ js/jump.js
@@ -0,0 +1,23 @@
+/*
+Copyright (C) 2022 dsy4567 <https://github.com/dsy4567 | dsy4567@outlook.com>
+此代码在 GPL-3.0 下获得许可
+*/
+
+const 网站被hx = 0;
+
+if (
+    location.pathname != "/" &&
+    location.pathname != "/index.html" &&
+    (网站被hx || location.href.includes("dnt=1")) &&
+    !location.href.includes("jump=1")
+)
+    location.href = "/?dnt=1";
+else if (
+    (location.pathname == "/" || location.pathname == "/index.html") &&
+    (location.href.includes("jump=1") || !网站被hx) &&
+    !location.href.includes("dnt=1")
+)
+    if (location.href.includes("jump=1"))
+        window[atob("bG9jYXRpb24=")][atob("aHJlZg==")] = atob("aS5odG1s") + "?jump=1";
+    else window[atob("bG9jYXRpb24=")][atob("aHJlZg==")] = atob("aS5odG1s");
+else document.title = document.getElementsByTagName("title")[0].getAttribute("t");
